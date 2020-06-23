e083bb8e2920a605c930f9ca872b7f7941041f2d
Henry Buisseret <henrybuisseret@gmail.com>
2020-06-23 16:55:45 +0200

    omponentWillMount 44'

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,30 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Flat = (props) => {
-  const style = {
-  backgroundImage: `url(${props.flat.imageUrl})`
-  };
+class Flat extends Component {
+  render() {
+    const style = {
+      backgroundImage: `url(${this.props.flat.imageUrl})`
+    };
 
-  return (
-    <div className="flat card-container">
-      <div className="card" style={style}>
-
-        <div className="card-trip">
-          <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/greece.jpg" />
-          <div className="card-trip-infos">
-            <div>
-              <h2></h2>
-              <p>Short description here!</p>
-            </div>
-            <h2 className="card-trip-pricing">£99.99</h2>
-            <img src="https://kitt.lewagon.com/placeholder/users/krokrob" className="card-trip-user avatar-bordered" />
+    return (
+        <div className="flat card" style={style}>
+          <div className="card-description">
+            <h1>{this.props.flat.name}</h1>
+            <h2>{this.props.flat.price}</h2>
           </div>
         </div>
-
-
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Flat;
